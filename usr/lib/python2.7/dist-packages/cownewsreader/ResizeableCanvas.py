8b858b5ebc9d7314b1ec960ee080c1294e42be26
blediboss <bossbledi@gmail.com>
2016-02-07 23:46:12 +0200

    Added a simple resizeable canvas

---
+++ usr/lib/python2.7/dist-packages/cownewsreader/ResizeableCanvas.py
@@ -0,0 +1,18 @@
+from Tkinter import *
+class ResizingCanvas(Canvas):
+    def __init__(self,parent,**kwargs):
+        Canvas.__init__(self,parent,**kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self,event):
+		# determine the ratio of old width/height to new width/height
+		wscale = float(event.width)/self.width
+		hscale = float(event.height)/self.height
+		self.width = event.width
+		self.height = event.height
+		# resize the canvas 
+		self.config(width=self.width, height=self.height)
+		# rescale all the objects tagged with the "all" tag
+		self.scale("all",0,0,wscale,hscale)
